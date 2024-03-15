f6125b24b60946aa35310211738a67b0a0fb2573
Siddharth Raghuvanshi <siddharthraghuvanshi@Siddharths-MacBook-Pro.local>
2024-03-15 13:52:14 +0530

    initial commit to spaceX-backend code

---
+++ spacex-backend/src/main/java/com/spacex/spacexbackend/SpacexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacex.spacexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacexBackendApplication.class, args);
+	}
+
+}
